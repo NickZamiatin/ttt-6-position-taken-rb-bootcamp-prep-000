2ca677079afc0b8b7e9ac38a6177d0e14d4b7cae
MYKYTA ZAMIATIN <4406015@gmail.com>
2017-11-25 21:36:36 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,18 @@
 # code your #position_taken? method here!
 
-board = [nil, " ", " ", " ", " ", " ", " ", " ", " "]
-index = 0
+board = [nil, "def position_taken?(board, index)
+  # basic solution:
+
+  # taken = nil
+  # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  #   taken = false
+  # else
+  #   taken = true
+  # end
+  # taken
+
+  # advanced solution w/ ternary operator
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
+
+end
